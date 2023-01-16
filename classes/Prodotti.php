e0417692add4d1553ba973b1779d37cc1fa7df49
luciobo <luciobo@icloud.com>
2023-01-16 16:55:23 +0100

    qualche passo in avanti

--- classes/Prodotti.php
+++ classes/Prodotti.php
@@ -1,10 +1,100 @@
-<?php 
+<?php
 
 
 class Prodotti {
     private $nomeProdotto;
     private $categoriaProdotto;
+    private $immagini;
+    private $prezzo;
 
+function __construct($_nomeProdotto, $_categoriaProdotto, $_prezzo ) {
+
+        $this->setNomeProdotto($_nomeProdotto);
+        $this->setCategoriaProdotto($_categoriaProdotto);
+        $this->setPrezzo($_prezzo);
 }
+
+
+    /**
+     * Get the value of nomeProdotto
+     */ 
+    public function getNomeProdotto()
+    {
+        return $this->nomeProdotto;
+    }
+
+    /**
+     * Set the value of nomeProdotto
+     *
+     * @return  self
+     */ 
+    public function setNomeProdotto($_nomeProdotto)
+    {
+        $this->nomeProdotto = $_nomeProdotto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoriaProdotto
+     */ 
+    public function getCategoriaProdotto()
+    {
+        return $this->categoriaProdotto;
+    }
+
+    /**
+     * Set the value of categoriaProdotto
+     *
+     * @return  self
+     */ 
+    public function setCategoriaProdotto($_categoriaProdotto)
+    {
+        $this->categoriaProdotto = $_categoriaProdotto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of immagini
+     */ 
+    public function getImmagini()
+    {
+        return $this->immagini;
+    }
+
+    /**
+     * Set the value of immagini
+     *
+     * @return  self
+     */ 
+    public function setImmagini($_immagini)
+    {
+        $this->immagini = $_immagini;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prezzo
+     */ 
+    public function getPrezzo()
+    {
+        return $this->prezzo;
+    }
+
+    /**
+     * Set the value of prezzo
+     *
+     * @return  self
+     */ 
+    public function setPrezzo($_prezzo)
+    {
+        $this->prezzo = $_prezzo;
+
+        return $this;
+    }
+}
+
 
 ?>
--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 require_once "./classes/Prodotti.php";
 
+$mieiProdotti = new Prodotti("cuccia per cani", "cuccie" , "40") ;
 
+var_dump($mieiProdotti);
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 
 <head>
     <meta charset="UTF-8">
@@ -19,7 +21,7 @@ require_once "./classes/Prodotti.php";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
